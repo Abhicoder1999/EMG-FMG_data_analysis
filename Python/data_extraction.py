8c129cf8efd4bff2498c102aac8ebe88cc3135f8
Abhijeet Tripathy <abhijeetofficial1999@gmail.com>
2019-12-24 13:09:57 +0530

    extract_data

---
+++ Python/data_extraction.py
@@ -0,0 +1,7 @@
+import pandas as pd
+filename = r'../Data/EMG_SUBHODIP_SPEED1.4_TRIAL1.xlsx'
+df_EMG = pd.read_excel (io = filename)
+
+filename = r'../Data/Speed_1.44_Trial1_FMG.xlsx'
+df_FMG = pd.read_excel (io = filename)
+
